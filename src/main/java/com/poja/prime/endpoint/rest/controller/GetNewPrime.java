18036653bf1df8d3a072a94ee8d7672c358e96c9
Abraham10000 <99530526+Abraham10000@users.noreply.github.com>
2023-12-05 19:19:17 +0200

    feat : add method to get new prime

---
+++ src/main/java/com/poja/prime/endpoint/rest/controller/GetNewPrime.java
@@ -0,0 +1,17 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigInteger;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/preprod")
+public class PrimeController {
+
+    @GetMapping("/new-prime")
+    public String getNewPrime() {
+        BigInteger prime = BigInteger.probablePrime(10_000, new Random());
+      
+        return prime.toString(); s
+    }
+}

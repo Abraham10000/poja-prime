20b4f9edd7cde5720be8b68f484833974a3a41b0
Abraham10000 <hei.abraham.4@gmail.com>
2023-12-05 21:25:06 +0200

    fix : PrimeController

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -3,6 +3,7 @@ package com.poja.prime.endpoint.rest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Random;
 
 @RestController
@@ -10,7 +11,15 @@ public class PrimeController {
 
     @GetMapping("/new-prime")
     public String getNewPrime() {
-        BigInteger prime = BigInteger.probablePrime(10_000, new Random());
+        int bitLength = 10000; 
+        SecureRandom random = new SecureRandom();
+
+        long startTime = System.currentTimeMillis();
+        BigInteger prime = BigInteger.probablePrime(bitLength, random);
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Temps de génération : " + (endTime - startTime) + " ms");
+
         return prime.toString();
     }
 }
